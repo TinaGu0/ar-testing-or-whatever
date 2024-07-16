389e35825dffda31f8926820c7ba907d63e84402
TinaGu0 <144460946+TinaGu0@users.noreply.github.com>
2024-07-16 09:52:44 -0400

    a

--- haha/main.js
+++ haha/main.js
@@ -1,1 +1,28 @@
 import * as THREE from 'three';
+//below is for text
+//import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 3, 3, 3 );
+const material = new THREE.MeshBasicMaterial( { color: "#FFF" } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function animate() {
+    cube.rotation.x += 1;
+    cube.rotation.y += 0.05;
+	renderer.render( scene, camera );
+}
+renderer.setAnimationLoop( animate );
+
+//3D model
+
